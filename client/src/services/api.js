e9e96a3da2ee92784a9c85838c5e24860bef704b
Swathi S <swathis.prof@gmail.com>
2026-05-16 18:23:23 +0530

    updated live backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://portfolio-project-87ns.onrender.com/api",
+  baseURL: "https://portfolio-project-87ns.onrender.com",
 });
 
 export default API;
